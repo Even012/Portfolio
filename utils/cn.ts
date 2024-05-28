7861a14c5e5e60ed7f726180d21e8e9e61028808
“Even-1” <z5395765@ad.unsw.edu.au>
2024-05-28 15:23:36 +1000

    initial commit

---
+++ utils/cn.ts
@@ -0,0 +1,8 @@
+// helper function to make everything work together with tailwind
+
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
