3918551d908ec1a8e65c9c4b19882283f4902d6d
“Even-1” <z5395765@ad.unsw.edu.au>
2024-06-11 14:27:01 +1000

    About: change the layout; Projects: remove the extra ones

--- components/About.tsx
+++ components/About.tsx
@@ -11,9 +11,8 @@ const About = () => {
             <h2 className="text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
                 Proactive and detail-oriented <span className='text-purple'>Computer Science student at UNSW</span>
             </h2>
-            <p className="mt-4 text-base/6 text-neutral-200 lg:py-5">
-                Proficient in programming languages including Python, C and JavaScript with a strong foundation in algorithms, machine
-                learning, and web development. Actively seeking an internship position where I can further develop my software
+            <p className="mt-4 text-lg text-neutral-200 lg:py-5">
+                Actively seeking an internship position where I can further develop my software
                 engineering skills and contribute to real-world technology solutions.
             </p>
 
@@ -26,7 +25,12 @@ const About = () => {
                 <h2 className="text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
                     My <span className='text-purple'>tech stack</span>
                 </h2>
-                <div className="flex gap-1 lg:gap-5 w-fit absolute right-3 lg:right-10">
+                <p className="mt-4 text-neutral-200 lg:py-5 text-lg">
+                  Proficient in programming languages including Python, C/C++ and JavaScript with a strong foundation in algorithms, machine
+                  learning, and web development. 
+                </p>
+                <div className="flex gap-1 lg:gap-5 w-fit absolute bottom-5 right-3 lg:right-10">
+
                   <div className="flex flex-col gap-3 lg:gap-8">
                     {['React.js', 'Next.js', 'TypeScript'].map((item) => (
                       <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
--- data/index.ts
+++ data/index.ts
@@ -83,25 +83,25 @@ export const navItems = [
       title: "Airbrb - Renting platform website modelled off Airbnb",
       des: "modell off the popular property renting platfrom Airbnb using ReactJS",
       img: "/p2.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/zoom-clone",
+      iconLists: ["/re.svg", "/tail.svg", "/js.svg", "/ex.svg", "/c.svg"],
+      link: "https://github.com/Even012/Renting-platform-Airbrb",
     },
-    {
-      id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
-    },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
-    },
+    // {
+    //   id: 3,
+    //   title: "AI Image SaaS - Canva Application",
+    //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    //   img: "/p3.svg",
+    //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    //   link: "https://github.com/adrianhajdin/ai_saas_app",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Animated Apple Iphone 3D Website",
+    //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    //   img: "/p4.svg",
+    //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    //   link: "https://github.com/adrianhajdin/iphone",
+    // },
   ];
   
   export const testimonials = [
